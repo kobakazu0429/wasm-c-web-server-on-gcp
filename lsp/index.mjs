37e9e7dab36bd17b83bada2f454a769a50e4e8c8
kobakazu0429 <kobakazu10@gmail.com>
2023-11-18 20:43:09 +0900

    feat: x-trace

--- lsp/index.mjs
+++ lsp/index.mjs
@@ -5,6 +5,26 @@ import * as rpc from "vscode-ws-jsonrpc";
 import * as server from "vscode-ws-jsonrpc/server";
 import * as lsp from "vscode-languageserver";
 import url from "node:url";
+import cp from "node:child_process";
+
+// from: vscode-ws-jsonrpc/server
+function createServerProcess(serverName, command, args, options, traceId) {
+  const serverProcess = cp.spawn(command, args || [], options || {});
+  serverProcess.on("error", (error) =>
+    console.error(`Launching Server failed: ${error}`)
+  );
+  if (serverProcess.stderr !== null) {
+    serverProcess.stderr.on("data", (data) => {
+      console.error({
+        lspLog: data.toString(),
+        "logging.googleapis.com/trace": traceId
+          ? `projects/wasm-c-web/traces/${traceId}`
+          : undefined,
+      });
+    });
+  }
+  return server.createProcessStreamConnection(serverProcess);
+}
 
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception: ", err.toString());
@@ -17,7 +37,7 @@ const CLANGD = process.env.CLANGD;
 if (!CLANGD) throw new Error("CLANGD not set");
 
 /** @type {(socket: rpc.IWebSocket) => void} */
-const launch = (socket) => {
+const launch = (socket, traceId) => {
   const reader = new rpc.WebSocketMessageReader(socket);
   const writer = new rpc.WebSocketMessageWriter(socket);
 
@@ -25,7 +45,13 @@ const launch = (socket) => {
     socket.dispose();
   });
 
-  const serverConnection = server.createServerProcess("c", CLANGD);
+  const serverConnection = createServerProcess(
+    "c",
+    CLANGD,
+    ["--log=verbose", "--pretty"],
+    {},
+    traceId
+  );
   if (!serverConnection) {
     throw new Error("serverConnection is undefined.");
   }
@@ -38,7 +64,6 @@ const launch = (socket) => {
         /** @type {lsp.InitializeParams} */
         const initializeParams = message.params;
         initializeParams.processId = process.pid;
-        console.log(message);
       }
     }
     return message;
@@ -60,7 +85,12 @@ const build = async () => {
     handler: (_req, reply) => {
       reply.send("pong");
     },
-    wsHandler: (connection, _req) => {
+    wsHandler: (connection, req) => {
+      // X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE
+      const trace = req.headers["x-cloud-trace-context"];
+      const [, traceId, _spanId, _traceTrue] =
+        /([a-f0-9]+)\/([a-f0-9]+);o=(0|1)/.exec(trace ?? "") ?? [];
+
       /** @type {rpc.IWebSocket} */
       const socket = {
         send: (content) => {
@@ -94,10 +124,10 @@ const build = async () => {
       if (connection.socket.readyState === connection.socket.OPEN) {
         // console.log("readyState: ", connection.socket.readyState);
         // console.log("connection.socket.OPEN:", connection.socket.OPEN);
-        launch(socket);
+        launch(socket, traceId);
       } else {
         // console.log("readyState: ", connection.socket.readyState);
-        connection.socket.on("open", () => launch(socket));
+        connection.socket.on("open", () => launch(socket, traceId));
       }
     },
   });
